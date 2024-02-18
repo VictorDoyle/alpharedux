f79bfb45606a3ea3a1e43ac963a5d258c9a26a08
VictorDoyle <victor-doyle@outlook.com>
2024-02-18 15:41:13 -0500

    adding tabs content to projects

--- src/app/components/projectTabs/ProjectTabs_RnD.tsx
+++ src/app/components/projectTabs/ProjectTabs_RnD.tsx
@@ -1,7 +1,48 @@
+'use client'
+import { SetStateAction, useState } from 'react';
+import styles from './RnDTab.module.css';
+
 export default function RnDTab() {
+    const rndTopics = [
+        'Black Hole Theory',
+        'Energy conservation in sound transference',
+        'Advanced Capitalist effect on consumer index',
+    ];
+    const [activeRndTopic, setActiveRndTopic] = useState(rndTopics[0]);
+
+    const handleRndTopicClick = (topic: SetStateAction<string>) => {
+        setActiveRndTopic(topic);
+    };
+
+    const renderRndDetails = () => {
+        switch (activeRndTopic) {
+            case 'Black Hole Theory':
+                return <div>Information on Black Hole Theory</div>;
+            case 'Energy conservation in sound transference':
+                return <div>Details on Energy Conservation in Sound Transference</div>;
+            case 'Advanced Capitalist effect on consumer index':
+                return <div>Analysis of Advanced Capitalist Effect on Consumer Index</div>;
+            default:
+                return <div>Select a topic to see the details.</div>;
+        }
+    };
+
     return (
-        <>
-            Research and Development Tab
-        </>
+        <div className={styles.rndTabPanel}>
+            <div className={styles.rndTopicList}>
+                {rndTopics.map((topic) => (
+                    <button
+                        key={topic}
+                        onClick={() => handleRndTopicClick(topic)}
+                        className={topic === activeRndTopic ? styles.activeTopic : styles.topic}
+                    >
+                        {topic}
+                    </button>
+                ))}
+            </div>
+            <div className={styles.topicDetails}>
+                {renderRndDetails()}
+            </div>
+        </div>
     );
-}
+};
--- src/app/components/projectTabs/ProjectTabs_VideoGames.tsx
+++ src/app/components/projectTabs/ProjectTabs_VideoGames.tsx
@@ -1,8 +1,48 @@
+'use client'
+import { SetStateAction, useState } from 'react';
+import styles from './VideoGameTab.module.css';
 
 export default function VideoGameTab() {
+    const videoGames = ['The Tower', '7 Minutes', 'The Black Dossier', 'The Anomaly', 'Rebirth',];
+    const [activeVideoGame, setActiveVideoGame] = useState(videoGames[0]);
+
+    const handleVideoGameClick = (game: SetStateAction<string>) => {
+        setActiveVideoGame(game);
+    };
+
+    const renderVideoGameDetails = () => {
+        switch (activeVideoGame) {
+            case 'The Tower':
+                return <div>The tower game</div>;
+            case '7 Minutes':
+                return <div>7 Minutes Game</div>;
+            case 'The Black Dossier':
+                return <div> The Black Dossier </div>;
+            case 'The Anomaly':
+                return <div>The Anomaly</div>;
+            case 'Rebirth':
+                return <div>Rebirth Game</div>;
+            default:
+                return <div>Select a game to see the details.</div>;
+        }
+    };
+
     return (
-        <>
-            Video Game Tab
-        </>
+        <div className={styles.videoGameTabPanel}>
+            <div className={styles.videoGameList}>
+                {videoGames.map((game) => (
+                    <button
+                        key={game}
+                        onClick={() => handleVideoGameClick(game)}
+                        className={game === activeVideoGame ? styles.activeGame : styles.game}
+                    >
+                        {game}
+                    </button>
+                ))}
+            </div>
+            <div className={styles.gameDetails}>
+                {renderVideoGameDetails()}
+            </div>
+        </div>
     );
-}
+};
